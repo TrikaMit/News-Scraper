34f909a9ca3d8346169f380828c0a3ef7523f99c
TrikaMit <trikamitra@gmail.com>
2018-08-29 23:38:52 -0400

    models working

---
+++ models/Article.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var ArticleSchema = new Schema({
+  // `title` is required and of type String
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  link: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  comment: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }
+});
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
--- server.js
+++ server.js
@@ -1,26 +1,61 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const axios = require("axios");
 const cheerio = require("cheerio");
 const request = require("request");
 
-request("https://www.huffingtonpost.com/section/world-news", function(error, response, html) {
-  const $ = cheerio.load(html);
-  const results = [];
-//   .children('div.card').each
+const db = require("./models");
 
-  $('div.card--original-center', 'section#zone-original-center').each(function(i,element) {
-    //   console.log(element)
-    let link = 'https://www.huffingtonpost.com'+$(element).children('div.card__content').children('a.card__image__wrapper').attr('href').trim();
-    let title = $(element).children('div.card__content').children('div.card__details').children('.card__headlines').children('.card__headline').text().trim();
-    let imgFull = $(element).children('div.card__content').children('a.card__image__wrapper').children('.card__image').children('img').attr('src').trim();
-    let img = imgFull.split("?cache")[0];
-    if (img.includes("?ops=")){
-        img = img.split("?ops=")[0];
-    }
-    results.push({
-      title: title,
-      link: link,
-      img: img
+var PORT = 3000;
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsscraper";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true
+});
+mongoose.set('useCreateIndex', true)
+
+
+axios.get("https://www.huffingtonpost.com/section/world-news").then(function (response) {
+    const $ = cheerio.load(response.data);
+    $('div.card--original-center').each(function (i, element) {
+        let result = {};
+
+        result.title = $(this).children('div.card__content').children('div.card__details').children('.card__headlines').children('.card__headline').text().trim();
+        result.link = 'https://www.huffingtonpost.com' + $(this).children('div.card__content').children('a.card__image__wrapper').attr('href').trim();
+
+        let image = $(this).children('div.card__content').children('a.card__image__wrapper').children('.card__image')
+            .children('img').attr('src').trim().split("?cache")[0];
+
+        image.includes("?ops=") ? result.image = image.split("?ops=")[0] : result.image = image;
+        
+
+        db.Article.create(result)
+        .then(function(dbArticle) {
+            console.log(dbArticle);
+        })
+        .catch(function(err){
+            return err;
+        })
+
+
+        
+        
+
+        //   db.create(result)
+        //   .then(function(article) {
+        //       console.log(article)
+        //   }).catch(function(err) {
+        //       return res.json(err);
+        //   })
     });
-  });
 
-  console.log(results);
 });
